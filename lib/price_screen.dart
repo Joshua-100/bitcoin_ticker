f5cb2310de4f030c59e54be692faeb9f43cdb772
mavrentech <mutenjejoshua@gmail.com>
2024-05-28 10:52:07 +0200

    Successefuly implemented the Cupertino Picker

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'coin_data.dart';
+
+const double _kItemExtent = 32.0;
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -41,36 +44,50 @@ class _PriceScreenState extends State<PriceScreen> {
             ),
           ),
           Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: DropdownButton<String>(
-              value: currency,
-              icon: const Icon(Icons.arrow_drop_down_sharp),
-              elevation: 16,
-              style: const TextStyle(color: Colors.white),
-              underline: Container(
-                height: 2,
-                color: Colors.white,
-              ),
-              onChanged: (String? value) {
-                // This is called when the user selects an item.
-                setState(() {
-                  currency = value!;
-                });
-              },
-              items:
-                  currenciesList.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-          ),
+              height: 150.0,
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(bottom: 30.0),
+              color: Colors.lightBlue,
+              child: CupertinoPicker(
+                itemExtent: _kItemExtent,
+                onSelectedItemChanged: (int value) {
+                  setState(() {
+                    currency = currenciesList[value];
+                    print(currency);
+                  });
+                },
+                children:
+                    List<Widget>.generate(currenciesList.length, (int index) {
+                  return Center(child: Text(currenciesList[index]));
+                }),
+              )),
         ],
       ),
     );
   }
 }
+
+//
+// DropdownButton<String>(
+// value: currency,
+// icon: const Icon(Icons.arrow_drop_down_sharp),
+// elevation: 16,
+// style: const TextStyle(color: Colors.white),
+// underline: Container(
+// height: 2,
+// color: Colors.white,
+// ),
+// onChanged: (String? value) {
+// // This is called when the user selects an item.
+// setState(() {
+// currency = value!;
+// });
+// },
+// items:
+// currenciesList.map<DropdownMenuItem<String>>((String value) {
+// return DropdownMenuItem<String>(
+// value: value,
+// child: Text(value),
+// );
+// }).toList(),
+// ),
