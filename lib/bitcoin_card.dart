946340f5a3c60e52e9c76420f06b1921f7742377
mavrentech <mutenjejoshua@gmail.com>
2024-05-28 17:15:32 +0200

    Refactored Bitcoin Card,

---
+++ lib/bitcoin_card.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class BitcoinCard extends StatelessWidget {
+  const BitcoinCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.lightBlueAccent,
+      elevation: 5.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+        child: Text(
+          '1 BTC = ? USD',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bitcoin_ticker/bitcoin_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'coin_data.dart';
@@ -24,24 +25,7 @@ class _PriceScreenState extends State<PriceScreen> {
         children: <Widget>[
           Padding(
             padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
+            child: BitcoinCard(),
           ),
           Container(
               height: 150.0,
